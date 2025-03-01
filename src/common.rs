babd68e21ed707d5009f0fcb4acbf5df0cc06059
a6856759 <103041228+a6856759@users.noreply.github.com>
2025-03-01 19:40:02 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://111.229.159.11:21114".to_owned()
+    "http://47.122.125.129:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
